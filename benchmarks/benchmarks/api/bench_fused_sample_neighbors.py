13317c850d40c3394842aeacca43dc40106e674a
ewaldherr <waldherr49@gmail.com>
2024-06-02 12:03:08 +0200

    test benchmark with different parameters

--- benchmarks/benchmarks/api/bench_fused_sample_neighbors.py
+++ benchmarks/benchmarks/api/bench_fused_sample_neighbors.py
@@ -16,10 +16,10 @@ from .. import utils
 @utils.parametrize("fanout", [5, 20, 40])
 def track_time(graph_name, seed_nodes_num, fanout):
     device = utils.get_bench_device()
-    graph = utils.get_graph(graph_name, "coo" ).to(device)
+    graph = utils.get_graph(graph_name, "coo").to(device)
 
     dgl.distributed.partition_graph(graph,graph_name,4,'tmp/test')
-    edge_dir = "out"
+    edge_dir = "in"
     for j in range(4):
         part_data = dgl.distributed.load_partition('tmp/test/' + graph_name + '.json', j)
         g, nfeat, efeat, partition_book, graph_name, ntypes, etypes = part_data
