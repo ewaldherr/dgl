4528fd4e808fd30a9f40396b343a53167e3e4801
ewaldherr <waldherr49@gmail.com>
2024-06-03 09:13:44 +0200

    fix: use distributed dgl lib for benchmarks

--- benchmarks/benchmarks/api/bench_nn_graphconv.py
+++ benchmarks/benchmarks/api/bench_nn_graphconv.py
@@ -19,18 +19,21 @@ from .. import utils
 def track_time(graph_name, feat_dim, aggr_type):
     device = utils.get_bench_device()
     graph = utils.get_graph(graph_name).to(device)
-    gg = dgl.transforms.metis_partition(graph,4)
-    feat = torch.randn((graph.num_nodes(), feat_dim), device=device)
-    model = SAGEConv(
-        feat_dim, feat_dim, aggr_type, activation=F.relu, bias=False
-    ).to(device)
+    dgl.distributed.partition_graph(graph,graph_name,4,'tmp/test')
+    for j in range(4):
+        part_data = dgl.distributed.load_partition('tmp/test/' + graph_name + '.json', j)
+        g, nfeat, efeat, partition_book, graph_name, ntypes, etypes = part_data
+        feat = torch.randn((g.num_nodes(), feat_dim), device=device)
+        model = SAGEConv(
+            feat_dim, feat_dim, aggr_type, activation=F.relu, bias=False
+        ).to(device)
 
-    # dry run
-    for i in range(3):
-        model(gg, feat)
-    # timing
-    with utils.Timer() as t:
-        for i in range(50):
-            model(gg, feat)
+        # dry run
+        for i in range(3):
+            model(g, feat)
+        # timing
+        with utils.Timer() as t:
+            for i in range(50):
+                model(g, feat)
 
-    return t.elapsed_secs / 50
+        return t.elapsed_secs / 50
