ec8afade2c9673d732e86e9008e3713985a03eb6
ewaldherr <waldherr49@gmail.com>
2024-06-03 09:19:21 +0200

    add node_subgraph benchmark

--- benchmarks/benchmarks/api/bench_node_subgraph.py
+++ benchmarks/benchmarks/api/bench_node_subgraph.py
@@ -11,24 +11,28 @@ from .. import utils
 
 @utils.benchmark("time")
 @utils.parametrize("graph_name", ["livejournal", "reddit"])
-@utils.parametrize("format", ["coo", "csc"])
 @utils.parametrize("seed_nodes_num", [200, 5000, 20000])
-def track_time(graph_name, format, seed_nodes_num):
+def track_time(graph_name, seed_nodes_num):
     device = utils.get_bench_device()
-    graph = utils.get_graph(graph_name, format)
+    graph = utils.get_graph(graph_name, "coo")
     graph = graph.to(device)
+    dgl.distributed.partition_graph(graph,graph_name,4,'tmp/test')
 
-    seed_nodes = np.random.randint(0, graph.num_nodes(), seed_nodes_num)
-    seed_nodes = torch.from_numpy(seed_nodes).to(device)
+    for j in range(4):
+        part_data = dgl.distributed.load_partition('tmp/test/' + graph_name + '.json', j)
+        g, nfeat, efeat, partition_book, graph_name, ntypes, etypes = part_data
 
-    # dry run
-    for i in range(3):
-        dgl.node_subgraph(graph, seed_nodes)
+        seed_nodes = np.random.randint(0, g.num_nodes(), seed_nodes_num)
+        seed_nodes = torch.from_numpy(seed_nodes).to(device)
 
-    # timing
-    num_iters = 50
-    with utils.Timer() as t:
-        for i in range(num_iters):
-            dgl.node_subgraph(graph, seed_nodes)
+        # dry run
+        for i in range(3):
+            dgl.node_subgraph(g, seed_nodes)
 
-    return t.elapsed_secs / num_iters
+        # timing
+        num_iters = 50
+        with utils.Timer() as t:
+            for i in range(num_iters):
+                dgl.node_subgraph(g, seed_nodes)
+
+        return t.elapsed_secs / num_iters
