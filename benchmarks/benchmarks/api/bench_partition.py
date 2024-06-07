5dd6ac06a57393e5ed098ae237d912fc64e9a50e
ewaldherr <waldherr49@gmail.com>
2024-06-07 15:42:18 +0200

    test: metis partition only

--- benchmarks/benchmarks/api/bench_partition.py
+++ benchmarks/benchmarks/api/bench_partition.py
@@ -11,7 +11,7 @@ from .. import utils
 @utils.skip_if_gpu()
 @utils.benchmark("time", timeout=1200)
 @utils.parametrize("graph_name", ["reddit"])
-@utils.parametrize("algorithm", ["kahip","metis"])
+#@utils.parametrize("algorithm", ["kahip","metis"])
 @utils.parametrize("k", [2, 4, 8])
 def track_time(graph_name,algorithm, k):
     device = utils.get_bench_device()
@@ -23,6 +23,6 @@ def track_time(graph_name,algorithm, k):
     # timing
     with utils.Timer() as t:
         for i in range(3):
-            dgl.distributed.partition_graph(graph,graph_name, k,"tmp/test",part_method=algorithm)
+            gg = dgl.transform.metis_partition_assignment(graph,k)
 
     return t.elapsed_secs / 3
