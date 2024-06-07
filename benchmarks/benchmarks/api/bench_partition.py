6c728576ffe7e9620413b374baf9b12757ac9446
ewaldherr <waldherr49@gmail.com>
2024-06-07 15:44:51 +0200

    typo again

--- benchmarks/benchmarks/api/bench_partition.py
+++ benchmarks/benchmarks/api/bench_partition.py
@@ -23,6 +23,6 @@ def track_time(graph_name, k):
     # timing
     with utils.Timer() as t:
         for i in range(3):
-            gg = dgl.transform.metis_partition_assignment(graph,k)
+            gg = dgl.transforms.metis_partition_assignment(graph,k)
 
     return t.elapsed_secs / 3
