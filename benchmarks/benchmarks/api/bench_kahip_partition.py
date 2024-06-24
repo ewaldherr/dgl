8ee2e2665efafc680a34c45dbb198b05d960d2ce
ewaldherr <waldherr49@gmail.com>
2024-06-24 14:12:55 +0200

    change used data set

--- benchmarks/benchmarks/api/bench_kahip_partition.py
+++ benchmarks/benchmarks/api/bench_kahip_partition.py
@@ -10,16 +10,12 @@ from .. import utils
 
 @utils.skip_if_gpu()
 @utils.benchmark("time", timeout=1200)
-@utils.parametrize("graph_name", ["fraud"])
-#@utils.parametrize("algorithm", ["kahip","metis"])
+@utils.parametrize("graph_name", ["cora"])
 @utils.parametrize("k", [2, 4, 8])
 def track_time(graph_name, k):
     device = utils.get_bench_device()
     data = utils.process_data(graph_name)
     graph = data[0]
-    # dry run
-    # gg = dgl.distributed.partition_graph(graph,graph_name, k,"tmp/test",part_method=algorithm)
-
     # timing
     with utils.Timer() as t:
         for i in range(3):
