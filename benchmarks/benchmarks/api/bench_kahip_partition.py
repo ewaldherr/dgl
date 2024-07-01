926685a6073944653a4380949b163c28b59866ed
ewaldherr <waldherr49@gmail.com>
2024-07-01 11:28:46 +0200

    edit distributed partitioning

--- benchmarks/benchmarks/api/bench_kahip_partition.py
+++ benchmarks/benchmarks/api/bench_kahip_partition.py
@@ -10,13 +10,12 @@ from .. import utils
 
 @utils.skip_if_gpu()
 @utils.benchmark("time", timeout=1200)
-@utils.parametrize("graph_name", ["cora","livejournal","ogbn-arxiv","friendster","pubmed","ogbn-products","reddit"])
-@utils.parametrize("k", [4])
+@utils.parametrize("graph_name", ["cora","pubmed","reddit"])
+@utils.parametrize("k", [2, 4, 8])
 def track_time(graph_name, k):
     device = utils.get_bench_device()
-    graph = utils.get_graph(graph_name, "coo")
-    graph = graph.to(device)
-
+    data = utils.process_data(graph_name)
+    graph = data[0]
     # timing
     with utils.Timer() as t:
         for i in range(3):
--- benchmarks/benchmarks/api/bench_partition.py
+++ benchmarks/benchmarks/api/bench_partition.py
@@ -11,7 +11,7 @@ from .. import utils
 @utils.skip_if_gpu()
 @utils.benchmark("time", timeout=1200)
 @utils.parametrize("graph_name", ["reddit"])
-#@utils.parametrize("algorithm", ["kahip","metis"])
+@utils.parametrize("algorithm", ["kahip","metis"])
 @utils.parametrize("k", [2, 4, 8])
 def track_time(graph_name, k):
     device = utils.get_bench_device()
@@ -23,6 +23,6 @@ def track_time(graph_name, k):
     # timing
     with utils.Timer() as t:
         for i in range(3):
-            dgl.distributed.partition_graph(graph,graph_name, k,"tmp/test")
+            dgl.distributed.partition_graph(graph,graph_name, k,"tmp/test",part_method=algorithm)
 
     return t.elapsed_secs / 3
