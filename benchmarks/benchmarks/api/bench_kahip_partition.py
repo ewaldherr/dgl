87c7bc8faa11f9d1f590fd1b4bbaa1ce01266a96
ewaldherr <waldherr49@gmail.com>
2024-07-01 10:51:42 +0200

    add multiple example graphs to kahip benchmark

--- benchmarks/benchmarks/api/bench_kahip_partition.py
+++ benchmarks/benchmarks/api/bench_kahip_partition.py
@@ -10,7 +10,7 @@ from .. import utils
 
 @utils.skip_if_gpu()
 @utils.benchmark("time", timeout=1200)
-@utils.parametrize("graph_name", ["cora","reddit"])
+@utils.parametrize("graph_name", ["cora","livejournal","ogbn-arxiv","friendster","pubmed","ogbn-products","reddit"])
 @utils.parametrize("k", [2, 4, 8])
 def track_time(graph_name, k):
     device = utils.get_bench_device()
