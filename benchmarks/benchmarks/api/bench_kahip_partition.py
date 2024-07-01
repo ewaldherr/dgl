ff9cdb8056864e82cbf34211fd6a567866ee75e0
ewaldherr <waldherr49@gmail.com>
2024-07-01 11:15:47 +0200

    benchmark with fixed k

--- benchmarks/benchmarks/api/bench_kahip_partition.py
+++ benchmarks/benchmarks/api/bench_kahip_partition.py
@@ -11,12 +11,12 @@ from .. import utils
 @utils.skip_if_gpu()
 @utils.benchmark("time", timeout=1200)
 @utils.parametrize("graph_name", ["cora","livejournal","ogbn-arxiv","friendster","pubmed","ogbn-products","reddit"])
-@utils.parametrize("k", [2, 4, 8])
+@utils.parametrize("k", [4])
 def track_time(graph_name, k):
     device = utils.get_bench_device()
     graph = utils.get_graph(graph_name, "coo")
     graph = graph.to(device)
-    
+
     # timing
     with utils.Timer() as t:
         for i in range(3):
