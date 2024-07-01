cf66143c4d077e9e6dcd79db6776b2ab8e0fd48d
ewaldherr <waldherr49@gmail.com>
2024-07-01 10:44:32 +0200

    edit benchmarks to run two graphs

--- benchmarks/benchmarks/api/bench_metis_partition.py
+++ benchmarks/benchmarks/api/bench_metis_partition.py
@@ -10,16 +10,13 @@ from .. import utils
 
 @utils.skip_if_gpu()
 @utils.benchmark("time", timeout=1200)
-@utils.parametrize("graph_name", ["reddit"])
-#@utils.parametrize("algorithm", ["kahip","metis"])
+@utils.parametrize("graph_name", ["cora","reddit"])
+
 @utils.parametrize("k", [2, 4, 8])
 def track_time(graph_name, k):
     device = utils.get_bench_device()
     data = utils.process_data(graph_name)
     graph = data[0]
-    # dry run
-    # gg = dgl.distributed.partition_graph(graph,graph_name, k,"tmp/test",part_method=algorithm)
-
     # timing
     with utils.Timer() as t:
         for i in range(3):
