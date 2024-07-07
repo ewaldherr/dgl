bfb891dd2715d92adabcd0fbd2d40553261f5ebb
ewaldherr <waldherr49@gmail.com>
2024-07-07 14:03:42 +0200

    edit khop benchmark

--- benchmarks/benchmarks/api/bench_khop.py
+++ benchmarks/benchmarks/api/bench_khop.py
@@ -9,20 +9,25 @@ from .. import utils
 
 
 @utils.benchmark("time", timeout=60)
-@utils.parametrize("graph_name", ["cora"])
-@utils.parametrize("format", ["coo", "csr"])
+@utils.parametrize("graph_name", ["cora","pubmed"])
+@utils.parametrize("vertex_weight",[True,False])
+@utils.parametrize("algorithm",["kahip","metis"])
 @utils.parametrize("k", [1, 3, 5])
-def track_time(graph_name, format, k):
+def track_time(graph_name, k, algorithm, vertex_weight):
     device = utils.get_bench_device()
-    graph = utils.get_graph(graph_name, format)
-    graph = graph.to(device)
-    graph = graph.formats([format])
-    # dry run
-    dgl.khop_graph(graph, k)
+    graph = utils.get_graph(graph_name, "coo").to(device)
+    graph = graph.formats("coo")
+    dgl.distributed.partition_graph(graph,graph_name,2,'tmp/test', part_method = algorithm, balance_edges = vertex_weight)
+    for j in range(4):
+        part_data = dgl.distributed.load_partition('tmp/test/' + graph_name + '.json', j)
+        g, nfeat, efeat, partition_book, graph_name, ntypes, etypes = part_data
 
-    # timing
-    with utils.Timer() as t:
-        for i in range(10):
-            gg = dgl.khop_graph(graph, k)
+        # dry run
+        dgl.khop_graph(g, k)
 
-    return t.elapsed_secs / 10
+        # timing
+        with utils.Timer() as t:
+            for i in range(10):
+                gg = dgl.khop_graph(g, k)
+
+        return t.elapsed_secs / 10
