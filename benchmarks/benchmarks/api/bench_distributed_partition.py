2aa9a69498c8f915901585ab25890647a456c956
ewaldherr <waldherr49@gmail.com>
2024-07-07 13:05:09 +0200

    edit partition benchmarks

---
+++ benchmarks/benchmarks/api/bench_distributed_partition.py
@@ -0,0 +1,27 @@
+import time
+
+import dgl
+
+import numpy as np
+import torch
+
+from .. import utils
+
+
+@utils.skip_if_gpu()
+@utils.benchmark("time", timeout=1200)
+@utils.parametrize("graph_name", ["cora","pubmed"])
+@utils.parametrize("vertex_weight",[True,False])
+@utils.parametrize("algorithm", ["kahip","metis"])
+@utils.parametrize("k", [2, 4, 8])
+def track_time(graph_name, k, algorithm, vertex_weight):
+    device = utils.get_bench_device()
+    data = utils.process_data(graph_name)
+    graph = data[0]
+
+    # timing
+    with utils.Timer() as t:
+        for i in range(3):
+            dgl.distributed.partition_graph(graph,graph_name, k,"tmp/test",part_method=algo, balance_edges = vertex_weight)
+
+    return t.elapsed_secs / 3
--- benchmarks/benchmarks/api/bench_partition.py
+++ benchmarks/benchmarks/api/bench_partition.py
@@ -11,16 +11,20 @@ from .. import utils
 @utils.skip_if_gpu()
 @utils.benchmark("time", timeout=1200)
 @utils.parametrize("graph_name", ["cora","pubmed"])
-@utils.parametrize("algo", ["kahip","metis"])
+@utils.parametrize("vertex_weight",[True,False])
+@utils.parametrize("algorithm",["kahip","metis"])
 @utils.parametrize("k", [2, 4, 8])
-def track_time(graph_name, k, algo):
+def track_time(graph_name, k, vertex_weight, algorithm):
     device = utils.get_bench_device()
     data = utils.process_data(graph_name)
     graph = data[0]
-
     # timing
-    with utils.Timer() as t:
-        for i in range(3):
-            dgl.distributed.partition_graph(graph,graph_name, k,"tmp/test",part_method=algo)
-
+    if algorithm == "kahip":
+        with utils.Timer() as t:
+            for i in range(3):
+                gg = dgl.transforms.kahip_partition_assignment(graph,k,balance_edges = vertex_weight)
+    else:
+        with utils.Timer() as t:
+            for i in range(3):
+                gg = dgl.transforms.metis_partition_assignment(graph,k,balance_edges = vertex_weight)
     return t.elapsed_secs / 3
