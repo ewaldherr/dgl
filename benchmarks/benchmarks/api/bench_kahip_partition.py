1ebb161fea6d8551c8135d54bceac6659f5a637e
ewaldherr <waldherr49@gmail.com>
2024-07-12 14:14:24 +0200

    add fastsocial mode to kahip partition

--- benchmarks/benchmarks/api/bench_kahip_partition.py
+++ benchmarks/benchmarks/api/bench_kahip_partition.py
@@ -13,13 +13,14 @@ from .. import utils
 @utils.parametrize("graph_name", ["cora","pubmed"])
 @utils.parametrize("vertex_weight",[True,False])
 @utils.parametrize("k", [2, 4, 8])
-def track_time(graph_name, k, vertex_weight):
+@utils.parametrize("kahip_mode", [1,3])
+def track_time(graph_name, k, vertex_weight, kahip_mode):
     device = utils.get_bench_device()
     data = utils.process_data(graph_name)
     graph = data[0]
     # timing
     with utils.Timer() as t:
         for i in range(3):
-            gg = dgl.transforms.kahip_partition_assignment(graph,k,balance_edges = vertex_weight)
+            gg = dgl.transforms.kahip_partition_assignment(graph,k,balance_edges = vertex_weight, mode = kahip_mode)
 
     return t.elapsed_secs / 3
