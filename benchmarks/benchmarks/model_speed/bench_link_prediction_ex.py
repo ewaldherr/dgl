54e589709b457146ecf105e47c255b0a11ed41f4
ewaldherr <waldherr49@gmail.com>
2024-08-03 17:35:41 +0200

    fix parallelization

--- benchmarks/benchmarks/model_speed/bench_link_prediction_ex.py
+++ benchmarks/benchmarks/model_speed/bench_link_prediction_ex.py
@@ -11,19 +11,19 @@ import torch.nn.functional as F
 from dgl.nn import SAGEConv
 import dgl.function as fn
 from sklearn.metrics import roc_auc_score
-from multiprocessing import Process
+from torch.multiprocessing import Process, Manager
 
 os.environ["DGLBACKEND"] = "pytorch"
 
 from .. import utils
 
+# ----------- 2. create model -------------- #
 # build a two-layer GraphSAGE model
 class GraphSAGE(nn.Module):
     def __init__(self, in_feats, h_feats):
         super(GraphSAGE, self).__init__()
         self.conv1 = SAGEConv(in_feats, h_feats, "mean")
         self.conv2 = SAGEConv(h_feats, h_feats, "mean")
-
     def forward(self, g, in_feat):
         h = self.conv1(g, in_feat)
         h = F.relu(h)
@@ -42,11 +42,9 @@ class MLPPredictor(nn.Module):
         super().__init__()
         self.W1 = nn.Linear(h_feats * 2, h_feats)
         self.W2 = nn.Linear(h_feats, 1)
-
     def apply_edges(self, edges):
         h = torch.cat([edges.src["h"], edges.dst["h"]], 1)
         return {"score": self.W2(F.relu(self.W1(h))).squeeze(1)}
-
     def forward(self, g, h):
         with g.local_scope():
             g.ndata["h"] = h
@@ -63,14 +61,11 @@ def compute_auc(pos_score, neg_score):
     labels = torch.cat([torch.ones(pos_score.shape[0]), torch.zeros(neg_score.shape[0])]).numpy()
     return roc_auc_score(labels, scores)
 
-def train_partition(part_id, graph_name, features, model, train_neg_g, train_pos_g, pred, train_g, test_neg_g, test_pos_g):
-    # Load the partition
-    part_data = dgl.distributed.load_partition('tmp/partitioned/' + graph_name + '.json', part_id)
+def train_partition(i, k, algorithm, vertex_weight, graph_name, train_g, train_pos_g, train_neg_g, features, model, pred, optimizer):
+    part_data = dgl.distributed.load_partition('tmp/partitioned/' + graph_name + '.json', i)
     g, nfeat, efeat, partition_book, graph_name, ntypes, etypes = part_data
-    #set up loss and optimizer
-    optimizer = torch.optim.Adam(itertools.chain(model.parameters(), pred.parameters()), lr=0.01)
+
     for epoch in range(100):
-        # Train on the partition
         # forward
         h = model(g, features[g.ndata[dgl.NID]])
 
@@ -87,9 +82,8 @@ def train_partition(part_id, graph_name, features, model, train_neg_g, train_pos
         loss.backward()
         optimizer.step()
 
-
 @utils.skip_if_gpu()
-@utils.benchmark("time", timeout=20)
+@utils.benchmark("time", timeout=1200)
 @utils.parametrize("graph_name", ["Cora","Citeseer","Pubmed"])
 @utils.parametrize("vertex_weight",[True,False])
 @utils.parametrize("algorithm", ["kahip","metis","kahip_fs"])
@@ -102,7 +96,6 @@ def track_time(k, algorithm, vertex_weight, graph_name):
     }
     graph = datasets[graph_name][0]
     
-
     # Split edge set for training and testing
     u, v = graph.edges()
     eids = np.arange(graph.num_edges())
@@ -111,7 +104,6 @@ def track_time(k, algorithm, vertex_weight, graph_name):
     train_size = graph.num_edges() - test_size
     test_pos_u, test_pos_v = u[eids[:test_size]], v[eids[:test_size]]
     train_pos_u, train_pos_v = u[eids[test_size:]], v[eids[test_size:]]
-
     # Find all negative edges and split them for training and testing
     adj = sp.coo_matrix((np.ones(len(u)), (u.numpy(), v.numpy())))
     adj_neg = 1 - adj.todense() - np.eye(graph.num_nodes())
@@ -119,32 +111,40 @@ def track_time(k, algorithm, vertex_weight, graph_name):
     neg_eids = np.random.choice(len(neg_u), graph.num_edges())
     test_neg_u, test_neg_v = neg_u[neg_eids[:test_size]], neg_v[neg_eids[:test_size]]
     train_neg_u, train_neg_v = neg_u[neg_eids[test_size:]], neg_v[neg_eids[test_size:]]
-
     train_g = dgl.remove_edges(graph, eids[:test_size])
     train_pos_g = dgl.graph((train_pos_u, train_pos_v), num_nodes=graph.num_nodes())
     train_neg_g = dgl.graph((train_neg_u, train_neg_v), num_nodes=graph.num_nodes())
     test_pos_g = dgl.graph((test_pos_u, test_pos_v), num_nodes=graph.num_nodes())
     test_neg_g = dgl.graph((test_neg_u, test_neg_v), num_nodes=graph.num_nodes())
-
     model = GraphSAGE(train_g.ndata["feat"].shape[1], 16)
     pred = DotPredictor()
     features = train_g.ndata['feat']
-
+    
     if algorithm == "kahip_fs":
-        dgl.distributed.partition_graph(graph,graph_name, k,"tmp/partitioned",part_method = "kahip", balance_edges = vertex_weight, mode = 3)
+        dgl.distributed.partition_graph(graph, graph_name, k, "tmp/partitioned", part_method="kahip", balance_edges=vertex_weight, mode=3)
     else:
-        dgl.distributed.partition_graph(graph,graph_name, k,"tmp/partitioned",part_method = algorithm, balance_edges = vertex_weight)
+        dgl.distributed.partition_graph(graph, graph_name, k, "tmp/partitioned", part_method=algorithm, balance_edges=vertex_weight)
+
+    # ----------- 3. set up loss and optimizer -------------- #
+
+    optimizer = torch.optim.Adam(itertools.chain(model.parameters(), pred.parameters()), lr=0.01)
 
     with utils.Timer() as t:
         for i in range(3):
+            processes = []
             for i in range(k):
-                processes = []
-                for part_id in range(k):
-                    p = Process(target=train_partition, args=(part_id, graph_name, features, model, train_pos_g, train_neg_g, pred, train_g, test_neg_g, test_pos_g))
-                    p.start()
-                    processes.append(p)
+                p = Process(target=train_partition, args=(i, k, algorithm, vertex_weight, graph_name, train_g, train_pos_g, train_neg_g, features, model, pred, optimizer))
+                p.start()
+                processes.append(p)
 
-                for p in processes:
-                    p.join()
-                
+            for p in processes:
+                p.join()
+
+            #check results #
+            with torch.no_grad():
+                h = model(train_g, train_g.ndata["feat"])
+                pos_score = pred(test_pos_g, h)
+                neg_score = pred(test_neg_g, h)
+                print("AUC", compute_auc(pos_score, neg_score))
+
     return t.elapsed_secs / 3
