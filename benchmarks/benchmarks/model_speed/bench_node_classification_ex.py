28b8b7e80649c1d80f07b64c2bfbdc972a50f062
ewaldherr <waldherr49@gmail.com>
2024-08-25 12:18:38 +0200

    remove prints

--- benchmarks/benchmarks/model_speed/bench_node_classification_ex.py
+++ benchmarks/benchmarks/model_speed/bench_node_classification_ex.py
@@ -37,13 +37,11 @@ def train(g, features, labels, train_mask, model, epochs=100, lr=0.01):
 
 
 def train_partition(part_id, graph_name, features, labels, train_mask, model):
-    print("training start: " + part_id)
     # Load the partition
     part_data = dgl.distributed.load_partition('tmp/partitioned/' + graph_name + '.json', part_id)
     g, nfeat, efeat, partition_book, graph_name, ntypes, etypes = part_data
     # Train on the partition
     train(g, features[g.ndata[dgl.NID]], labels[g.ndata[dgl.NID]], train_mask[g.ndata[dgl.NID]], model)
-    print("finished: " + part_id)
     
 
 @utils.skip_if_gpu()
@@ -53,7 +51,7 @@ def train_partition(part_id, graph_name, features, labels, train_mask, model):
 #@utils.parametrize("algorithm", [-1,0,1,2,3,4,5])
 @utils.parametrize("k", [64])
 @utils.parametrize("vertex_weight",[True])
-@utils.parametrize("algorithm", [-1])
+@utils.parametrize("algorithm", [1])
 #@utils.parametrize("k", [16,32,64])
 def track_time(k, algorithm, vertex_weight, graph_name):
     datasets = {
@@ -65,7 +63,6 @@ def track_time(k, algorithm, vertex_weight, graph_name):
     features = graph.ndata['feat']
     labels = graph.ndata['label']
     train_mask = graph.ndata['train_mask']
-    print("finished loading")
     # Create model
     model = GCN(graph.ndata['feat'].shape[1], 16, len(torch.unique(labels)))
 
@@ -74,7 +71,6 @@ def track_time(k, algorithm, vertex_weight, graph_name):
         dgl.distributed.partition_graph(graph, graph_name, k, "tmp/partitioned", part_method="metis", balance_edges=vertex_weight)
     else:
         dgl.distributed.partition_graph(graph, graph_name, k, "tmp/partitioned", part_method="kahip", balance_edges=vertex_weight, mode=algorithm)
-    print("finished partitioning")
     # timing
     with utils.Timer() as t:
         for i in range(1):
