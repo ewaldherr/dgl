a87c75f036ad19f24bde821d821051a30a158419
ewaldherr <waldherr49@gmail.com>
2024-08-26 17:51:33 +0200

    fix score calculation

--- benchmarks/benchmarks/model_speed/bench_node_classification.py
+++ benchmarks/benchmarks/model_speed/bench_node_classification.py
@@ -1,6 +1,6 @@
 import time
 import os
-from multiprocessing import Process
+from multiprocessing import Process, Manager
 
 os.environ["DGLBACKEND"] = "pytorch"
 import dgl
@@ -35,16 +35,16 @@ def train(g, features, labels, train_mask, test_mask, model, epochs=100, lr=0.01
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-    return (pred[test_mask] == labels[test_mask]).float().mean()
+    return (pred[test_mask] == labels[test_mask]).float().mean().item()
 
 
-def train_partition(part_id, graph_name, features, labels, train_mask,test_mask, model):
+def train_partition(part_id, graph_name, features, labels, train_mask, test_mask, model, result_dict):
     # Load the partition
     part_data = dgl.distributed.load_partition('tmp/partitioned/' + graph_name + '.json', part_id)
     g, nfeat, efeat, partition_book, graph_name, ntypes, etypes = part_data
     # Train on the partition
-    return train(g, features[g.ndata[dgl.NID]], labels[g.ndata[dgl.NID]], train_mask[g.ndata[dgl.NID]],test_mask[g.ndata[dgl.NID]] model)
-    
+    result_dict[part_id] = train(g, features[g.ndata[dgl.NID]], labels[g.ndata[dgl.NID]], train_mask[g.ndata[dgl.NID]], test_mask[g.ndata[dgl.NID]], model)
+
 
 @utils.skip_if_gpu()
 @utils.benchmark("time", timeout=1200)
@@ -71,6 +71,7 @@ def track_time(k, algorithm, vertex_weight, graph_name):
     # timing
     part_time = 0
     score = 0
+
     with utils.Timer() as t:
         for i in range(3):
             # Partition the graph
@@ -80,13 +81,19 @@ def track_time(k, algorithm, vertex_weight, graph_name):
                 else:
                     dgl.distributed.partition_graph(graph, graph_name, k, "tmp/partitioned", part_method="kahip", balance_edges=vertex_weight, mode=algorithm)
                 part_time = t.elapsed()
+
+            manager = Manager()
+            result_dict = manager.dict()
             processes = []
             for part_id in range(k):
-                p = Process(target=train_partition, args=(part_id, graph_name, features, labels, train_mask,test_mask, model))
-                score += p.start()
+                p = Process(target=train_partition, args=(part_id, graph_name, features, labels, train_mask, test_mask, model, result_dict))
+                p.start()
                 processes.append(p)
 
             for p in processes:
                 p.join()
+
+            # Aggregate the results
+            score += sum(result_dict.values())
                 
-    return (t.elapsed_secs-part_time) / 3, part_time, score/(3*k)
+    return (t.elapsed_secs - part_time) / 3, part_time, score / (3 * k)
